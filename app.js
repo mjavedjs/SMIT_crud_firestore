4f97d5470de7d5ec493271f3aac9a0a6f19e1925
mjavedjs <mjavedjs@2006@gmail.com>
2025-01-25 12:33:01 +0500

    basic_cruid_applicatoin

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getFirestore, getDocs, doc,addDoc  ,collection,deleteDoc} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js"; 
+import { getFirestore, getDocs, doc,addDoc  ,collection,deleteDoc,getDoc,updateDoc} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js"; 
 // Ensure you're using "firebase-firestore.js" for Firestore functions
 
 // Your web app's Firebase configuration
@@ -25,21 +25,19 @@ form.addEventListener('submit', async (event) => {
     let title = document.getElementById('title').value;
   let description = document.getElementById('description').value;
 
+   // to empty input filed function
 
     function toEmptyinputFiled(){
        title = document.querySelector("#title").value = '';
        description = document.querySelector("#description").value = '';
-   }
-    // console.log("Title:", title);
-    // console.log("Description:", description);
-    // alert("Form Submitted Successfully!");
-
+   };
+       // to add data from user click 
     try {
         const docRef = await addDoc(collection(db, "users"), {
          title:title,
          description:description
         });
-        console.log("Document written with ID: ", docRef.id);
+        // console.log("Document written with ID: ", docRef.id);
       
       } catch (e) {
         console.error("Error adding document: ", e);
@@ -48,9 +46,15 @@ form.addEventListener('submit', async (event) => {
        toEmptyinputFiled()
   });
 
+
+
+  // render data on screen 
+
  async function renderData(){
    
     try {
+
+
      const docRef = await getDocs(collection(db,'users'));
 
       let html = ""
@@ -58,13 +62,14 @@ form.addEventListener('submit', async (event) => {
 
      docRef.forEach(element => {
          const userData = element.data();
-        const docid = element.id;
+         const docid = element.id; // jese he data database sa aye ga os ke aik id hoge jise docId kha ta hi
         //  console.log(globalArray);
           html+= `
           <tr>
           <td>${userData.title}</td>
           <td>${userData.description}</td>
            <td> <button id="delbtn" onclick="deletMethod('${docid}')">Delete</button></td>
+           <td> <button id="delbtn" onclick="UpdatedtMethod('${docid}')">UPDate</button></td>
            </tr>
        `
      });
@@ -79,9 +84,46 @@ form.addEventListener('submit', async (event) => {
 }
 
 renderData()
-
+// delet data 
 window.deletMethod = async function(docid){
   const docRef = doc(db,'users',docid);
   await deleteDoc(docRef)
   renderData()
+}
+
+// updated data from form 
+window.UpdatedtMethod =  async function(id){
+   try {
+    const docRef = await  getDoc(doc(db,'users',id))
+    const currentUser = docRef.data();
+
+    console.log(currentUser) // object is return from db is obj ma title or desction hai ;
+
+      document.querySelector("#title").value = currentUser.title;
+      document.querySelector("#description").value =currentUser.description;
+
+      const sumbitbtn = document.querySelector('.sumbit');
+       const btn = document.querySelector(".updatedbtn");
+       btn.classList.add('show');
+      sumbitbtn.classList.add('hide');
+
+      btn.addEventListener('click', async()=>{
+          const newtilte = document.querySelector("#title").value;
+          const newdescription = document.querySelector("#description").value;
+
+          if(!newtilte == null && !newdescription == null){
+              await updateDoc(doc(db,'users',id),{
+                   title:newtilte,
+                   description:newdescription
+              })
+              renderData()
+              btn.classList.remove('show');
+              sumbitbtn.classList.remove('hide');
+          }
+          
+      })
+
+   } catch (error) {
+     console.log(error)
+   }
 }
